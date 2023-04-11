81e9b2fff267b55099a7b2d361aa1f8603767015
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-11 16:54:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  for(let i in sampleObject){
+	  if(i == key) return true;
+  }
+	return false;
 }
 
 // Do not change the code below
 const key = prompt("Enter Key.");
-alert(hasKey(key));
+
